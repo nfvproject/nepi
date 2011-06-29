2dbf3f1506d3ca4c9ac9fc186bbe4208eb67c06c
cdfreire <cdfreire@antar.inria.fr>
2011-06-29 11:04:00 +0200

    Fix tests that expected root user

--- test/core/integration.py
+++ test/core/integration.py
@@ -198,7 +198,7 @@ class ExecuteTestCase(unittest.TestCase):
                 6: dict({ # element guid
                     'fake': dict({ # trace_id
                         'host': 'localhost', 
-                        'user': 'root', 
+                        'user': os.getlogin(), 
                         'filepath': '<test>'
                         })
                     })
