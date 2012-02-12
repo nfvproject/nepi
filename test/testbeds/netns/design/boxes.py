ed08f77029f451b8e11c98287c2ffbc7f088ed2a
alina <alina@localhost>
2012-02-12 18:55:23 +0100

    Bug fixes!

--- test/testbeds/netns/design/boxes.py
+++ test/testbeds/netns/design/boxes.py
@@ -21,11 +21,11 @@ class NETNSDesignBoxesTestCase(unittest.TestCase):
         node1.c.ifaces.connect(iface1.c.node)
         node2.c.ifaces.connect(iface2.c.node)
 
-        ip1 = provider.create("netns::IPAddress", container = emu)
+        ip1 = provider.create("netns::IP4Address", container = emu)
         ip1.a.address.value = "10.0.0.1"
         ip1.c.iface.connect(iface1.c.addrs)
 
-        ip2 = provider.create("netns::IPAddress", container = emu)
+        ip2 = provider.create("netns::IP4Address", container = emu)
         ip2.a.address.value = "10.0.0.2"
         ip2.c.iface.connect(iface2.c.addrs)
 
@@ -41,13 +41,35 @@ class NETNSDesignBoxesTestCase(unittest.TestCase):
         xml = exp.xml
         exp2 = provider.from_xml(xml)
         xml2 = exp2.xml
-        f = open('/tmp/xml1', 'w')
-        f.write(xml)
-        f.close()
-        f = open('/tmp/xml2', 'w')
-        f.write(xml2)
-        f.close()
         self.assertTrue(xml == xml2)
+       
+    def test_address_and_routes(self):
+        provider = create_provider()
+        exp = provider.create("Experiment")
+        emu = provider.create("netns::Emulation", container = exp)
+        emu.a.enableDebug.value = True
         
+        node1 = provider.create("netns::Node", container = emu)
+        node2 = provider.create("netns::Node", container = emu)
+
+        iface1 = provider.create("netns::P2PNodeInterface", container = emu)
+        iface1.a.up.value = True
+        iface2 = provider.create("netns::P2PNodeInterface", container = emu)
+        iface2.a.up.value = True
+
+        iface1.c.p2p.connect(iface2.c.p2p)
+
+        route1 = node1.add_route(destination = "0.0.0.0", nexthop = "192.168.0.1")
+        route2 = node2.add_route(destination = "0.0.0.0", nexthop = "192.168.0.2")
+
+        addr1 = iface1.add_address(address = "192.168.0.1")
+        addr2 = iface2.add_address(address = "192.168.0.2")
+
+        xml = exp.xml
+        exp2 = provider.from_xml(xml)
+        xml2 = exp2.xml
+        self.assertTrue(xml == xml2)
+
+
 if __name__ == '__main__':
     unittest.main()
--- test/testbeds/planetlab/design/boxes.py
+++ test/testbeds/planetlab/design/boxes.py
@@ -20,6 +20,7 @@ class PlanetLabDesignBoxesTestCase(unittest.TestCase):
         inet = provider.create("pl::Internet", container = sl)
         iface1.c.inet.connect(inet.c.ifaces)
         iface2.c.inet.connect(inet.c.ifaces)
+
         app = provider.create("pl::Application", container = sl)
         app.a.command.value = "ping -qc10 10.0.0.2"
         app.c.node.connect(node1.c.apps)
@@ -27,12 +28,6 @@ class PlanetLabDesignBoxesTestCase(unittest.TestCase):
         xml = exp.xml
         exp2 = provider.from_xml(xml)
         xml2 = exp2.xml
-        f = open('/tmp/xml1', 'w')
-        f.write(xml)
-        f.close()
-        f = open('/tmp/xml2', 'w')
-        f.write(xml2)
-        f.close()
         self.assertTrue(xml == xml2)
  
     def test_design_constrained(self):
@@ -50,12 +45,6 @@ class PlanetLabDesignBoxesTestCase(unittest.TestCase):
         xml = exp.xml
         exp2 = provider.from_xml(xml)
         xml2 = exp2.xml
-        f = open('/tmp/xml1', 'w')
-        f.write(xml)
-        f.close()
-        f = open('/tmp/xml2', 'w')
-        f.write(xml2)
-        f.close()
         self.assertTrue(xml == xml2)
  
     def test_design_constrained2(self):
@@ -75,12 +64,6 @@ class PlanetLabDesignBoxesTestCase(unittest.TestCase):
         xml = exp.xml
         exp2 = provider.from_xml(xml)
         xml2 = exp2.xml
-        f = open('/tmp/xml1', 'w')
-        f.write(xml)
-        f.close()
-        f = open('/tmp/xml2', 'w')
-        f.write(xml2)
-        f.close()
         self.assertTrue(xml == xml2)
 
         
@@ -106,13 +89,33 @@ class PlanetLabDesignBoxesTestCase(unittest.TestCase):
         xml = exp.xml
         exp2 = provider.from_xml(xml)
         xml2 = exp2.xml
-        f = open('/tmp/xml1', 'w')
-        f.write(xml)
-        f.close()
-        f = open('/tmp/xml2', 'w')
-        f.write(xml2)
-        f.close()
         self.assertTrue(xml == xml2)
+
+    def test_address_and_routes(self):
+        provider = create_provider()
+        exp = provider.create("Experiment")
+        sl = provider.create("pl::Slice", container = exp)
+        sl.a.slice.value = "inria_nepi"
+       
+        node1 = provider.create("pl::Node", container = sl)
+        node2 = provider.create("pl::Node", container = sl)
+        
+        iface1 = provider.create("pl::TapInterface", container = sl)
+        node1.c.ifaces.connect(iface1.c.node)
+        iface2 = provider.create("pl::TapInterface", container = sl)
+        node1.c.ifaces.connect(iface2.c.node)
+
+        route1 = node1.add_route(destination = "0.0.0.0", nexthop = "192.168.0.1")
+        route2 = node2.add_route(destination = "0.0.0.0", nexthop = "192.168.0.2")
+
+        addr1 = iface1.add_address(address = "192.168.0.1")
+        addr2 = iface2.add_address(address = "192.168.0.2")
+
+        xml = exp.xml
+        exp2 = provider.from_xml(xml)
+        xml2 = exp2.xml
+        self.assertTrue(xml == xml2)
+
 
 if __name__ == '__main__':
     unittest.main()
