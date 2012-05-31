6e7d2fab3733deba72f3733309dc7bed57572426
cdfreire <cdfreire@localhost>
2012-05-31 03:19:23 -0300

    Several small fixes to logging queue:  * Separate forward and backward queue logs  * Add an "interval" so that both immediate and timed measurements are possible  * Wake up logging thread on append when doing immediate logging  * Properly implement the outpath-injected args attribute  * Add missing newlines in csv log

--- src/nepi/testbeds/planetlab/scripts/loggingclassqueue.py
+++ src/nepi/testbeds/planetlab/scripts/loggingclassqueue.py
@@ -9,6 +9,7 @@ import time
 import classqueue
 
 _outpath = "output"
+_interval = 0
 
 class QueueLogger(threading.Thread):
     def __init__(self, queues, drops, accepts, outpath):
@@ -22,17 +23,22 @@ class QueueLogger(threading.Thread):
         self._terminate = False
     
     def run(self):
+        if _interval > 0:
+            interval = _interval
+        else:
+            interval = 1
+        
         t0 = time.time()
-        with open(self.outpath, "w+") as outfile:
-            outfile.write(",".join(
+        with open(self.outpath, "w") as outfile:
+            outfile.writelines((",".join(
                 ["time"]
                 + map("q%02dlen".__mod__, xrange(len(self.queues)))
                 + map("q%02ddrops".__mod__, xrange(len(self.queues)))
                 + map("q%02daccepts".__mod__, xrange(len(self.queues)))
-            ))
+            ), "\n"))
             
             while not self._terminate:
-                self._event.wait(1)
+                self._event.wait(interval)
                 if self._terminate:
                     break
                 
@@ -46,12 +52,12 @@ class QueueLogger(threading.Thread):
                 queueaccepts = list(self.accepts)
                 self.accepts[:] = [0] * len(self.accepts)
                 
-                outfile.write(",".join(
-                    [str(time.time)]
+                outfile.writelines((",".join(
+                    [str(time.time()-t0)]
                     + map(str, queuelens)
                     + map(str, queuedrops)
                     + map(str, queueaccepts)
-                ))
+                ), "\n"))
                 outfile.flush()
             
     def terminate(self):
@@ -62,13 +68,15 @@ class QueueLogger(threading.Thread):
         self._event.set()
 
 class LoggingClassQueue(classqueue.ClassQueue):
+    outpath_suffix = itertools.cycle(('_f','_b'))
+    
     def __init__(self):
         self.accepts = []
         self.drops = []
         super(LoggingClassQueue, self).__init__()
         
         # Prepare logger thread
-        self.logger = QueueLogger(self.queues, self.drops, self.accepts, _outpath)
+        self.logger = QueueLogger(self.queues, self.drops, self.accepts, _outpath+self.outpath_suffix.next())
         self.logger.start()
     
     def __del__(self):
@@ -88,11 +96,15 @@ class LoggingClassQueue(classqueue.ClassQueue):
         else:
             self.accepts[qi] += 1
         
+        if _interval == 0:
+            self.logger.wakeup()
+        
         return dropped
 
 queueclass = LoggingClassQueue
 
-def init(outpath="output", **kw):
-    global _outpath
+def init(outpath="output", interval=0, **kw):
+    global _outpath, _interval
     _outpath = outpath
+    _interval = interval
     classqueue.init(**kw)
