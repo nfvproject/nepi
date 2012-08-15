ed2391a8f29e8253de0d4ee14d861d65b78ac32a
alina <alina@localhost>
2012-08-15 18:22:33 +0200

    Updating nepi url in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         version     = "0.2",
         description = "High-level abstraction for running network experiments",
         author      = "Mathieu Lacage, Alina Quereilhac, Martín Ferrari and Claudio Freire",
-        url         = "http://nepi.pl.sophia.inria.fr/code/nepi",
+        url         = "http://nepi.inria.fr/code/nepi",
         license     = "GPLv2",
         platforms   = "Linux",
         packages    = [
