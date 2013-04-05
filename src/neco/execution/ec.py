27bf5bf20dd5ddf94225d9d791f1ec7dbacdd3e7
alina <alina@localhost>
2013-04-06 00:41:36 +0200

    Adding start_with_condition, stop_with_condition and set_with_condition to the EC (xxx: UNTESTED!!!)

--- src/neco/execution/ec.py
+++ src/neco/execution/ec.py
@@ -6,9 +6,12 @@ import threading
 
 from neco.util import guid
 from neco.util.timefuncs import strfnow, strfdiff, strfvalid 
-from neco.execution.resource import ResourceFactory
+from neco.execution.resource import ResourceFactory, ResourceAction, \
+        ResourceState
 from neco.execution.scheduler import HeapScheduler, Task, TaskStatus
 from neco.util.parallel import ParallelRun
+
+# TODO: use multiprocessing instead of threading
 
 class ExperimentController(object):
     def __init__(self, root_dir = "/tmp", loglevel = 'error'): 
@@ -75,46 +78,43 @@ class ExperimentController(object):
         rm1.connect(guid2)
         rm2.connect(guid1)
 
-    def discover_resource(self, guid, filters):
+    def register_condition(self, group1, action, group2, state,
+            time = None):
+        """ Registers an action START or STOP for all RM on group1 to occur 
+            time 'time' after all elements in group2 reached state 'state'.
+
+            :param group1: List of guids of RMs subjected to action
+            :type group1: list
+
+            :param action: Action to register (either START or STOP)
+            :type action: ResourceAction
+
+            :param group2: List of guids of RMs to we waited for
+            :type group2: list
+
+            :param state: State to wait for on RMs (STARTED, STOPPED, etc)
+            :type state: ResourceState
+
+            :param time: Time to wait after group2 has reached status 
+            :type time: string
+
+        """
+        if isinstance(group1, int):
+            group1 = list[group1]
+        if isinstance(group2, int):
+            group2 = list[group2]
+
+        for guid1 in group1:
+            rm = self.get_resource(guid)
+            rm.register_condition(action, group2, state, time)
+
+    def discover(self, guid, filters):
         rm = self.get_resource(guid)
         return rm.discover(filters)
 
-    def provision_resource(self, guid, filters):
+    def provision(self, guid, filters):
         rm = self.get_resource(guid)
         return rm.provision(filters)
-
-    def register_start(self, group1, time, after_status, group2):
-        if isinstance(group1, int):
-            group1 = list[group1]
-        if isinstance(group2, int):
-            group2 = list[group2]
-
-        for guid1 in group1:
-            for guid2 in group2:
-                rm = self.get_resource(guid)
-                rm.start_after(time, after_status, guid2)
-
-    def register_stop(self, group1, time, after_status, group2):
-        if isinstance(group1, int):
-            group1 = list[group1]
-        if isinstance(group2, int):
-            group2 = list[group2]
-
-        for guid1 in group1:
-            for guid2 in group2:
-                rm = self.get_resource(guid)
-                rm.stop_after(time, after_status, guid2)
-
-    def register_set(self, name, value, group1, time, after_status, group2):
-        if isinstance(group1, int):
-            group1 = list[group1]
-        if isinstance(group2, int):
-            group2 = list[group2]
-
-        for guid1 in group1:
-            for guid2 in group2:
-                rm = self.get_resource(guid)
-                rm.set_after(name, value, time, after_status, guid2)
 
     def get(self, guid, name):
         rm = self.get_resource(guid)
@@ -124,15 +124,83 @@ class ExperimentController(object):
         rm = self.get_resource(guid)
         return rm.set(name, value)
 
-    def status(self, guid):
+    def state(self, guid):
         rm = self.get_resource(guid)
-        return rm.status()
+        return rm.state
 
     def stop(self, guid):
         rm = self.get_resource(guid)
         return rm.stop()
 
-    def deploy(self, group = None, start_when_all_ready = True):
+    def start(self, guid):
+        rm = self.get_resource(guid)
+        return rm.start()
+
+    def set_with_conditions(self, name, value, group1, group2, state,
+            time = None):
+        """ Set value 'value' on attribute with name 'name' on all RMs of
+            group1 when 'time' has elapsed since all elements in group2 
+            have reached state 'state'.
+
+            :param name: Name of attribute to set in RM
+            :type name: string
+
+            :param value: Value of attribute to set in RM
+            :type name: string
+
+            :param group1: List of guids of RMs subjected to action
+            :type group1: list
+
+            :param action: Action to register (either START or STOP)
+            :type action: ResourceAction
+
+            :param group2: List of guids of RMs to we waited for
+            :type group2: list
+
+            :param state: State to wait for on RMs (STARTED, STOPPED, etc)
+            :type state: ResourceState
+
+            :param time: Time to wait after group2 has reached status 
+            :type time: string
+
+        """
+        if isinstance(group1, int):
+            group1 = list[group1]
+        if isinstance(group2, int):
+            group2 = list[group2]
+
+        for guid1 in group1:
+            rm = self.get_resource(guid)
+            rm.set_with_conditions(name, value, group2, state, time)
+
+    def stop_with_conditions(self, guid):
+        rm = self.get_resource(guid)
+        return rm.stop_with_conditions()
+
+    def start_with_conditions(self, guid):
+        rm = self.get_resource(guid)
+        return rm.start_with_condition()
+
+    def deploy(self, group = None, wait_all_ready = True):
+        """ Deploy all resource manager in group
+
+        :param group: List of guids of RMs to deploy
+        :type group: list
+
+        :param wait_all_ready: Wait until all RMs are deployed in
+            order to start the RMs
+        :type guid: int
+
+        """
+        def steps(rm):
+            rm.deploy()
+            rm.start_with_conditions()
+
+            # Only if the RM has STOP consitions we
+            # schedule a stop. Otherwise the RM will stop immediately
+            if rm.conditions.get(ResourceAction.STOP):
+                rm.stop_with_conditions()
+
         if not group:
             group = self.resources
 
@@ -140,13 +208,13 @@ class ExperimentController(object):
         for guid in group:
             rm = self.get_resource(guid)
 
-            kwargs = {'target': rm.deploy}
-            if start_when_all_ready:
+            if wait_all_ready:
                 towait = list(group)
                 towait.remove(guid)
-                kwargs['args'] = towait
+                self.register_condition(guid, ResourceAction.START, 
+                        towait, ResourceState.DEPLOYED)
 
-            thread = threading.Thread(kwargs)
+            thread = threading.Thread(target = steps, args = (rm))
             threads.append(thread)
             thread.start()
 
--- src/neco/resources/linux/node.py
+++ src/neco/resources/linux/node.py
@@ -1,5 +1,8 @@
-from neco.execution.resource import ResourceManager, clsinit
 from neco.execution.attribute import Attribute, Flags
+from neco.execution.resource import ResourceManager, clsinit, ResourceState
+from neco.resources.linux.ssh_api import SSHApiFactory
+
+import logging
 
 @clsinit
 class LinuxNode(ResourceManager):
@@ -10,42 +13,93 @@ class LinuxNode(ResourceManager):
         hostname = Attribute("hostname", "Hostname of the machine")
         username = Attribute("username", "Local account username", 
                 flags = Flags.Credential)
-        password = Attribute("pasword", "Local account password",
+        identity = Attribute("identity", "SSH identity file",
                 flags = Flags.Credential)
+        clean_home = Attribute("cleanHome", "Remove all files and directories 
+                from home folder before starting experiment", 
+                flags = Flags.ReadOnly)
+        clean_processes = Attribute("cleanProcesses", 
+                "Kill all running processes before starting experiment", 
+                flags = Flags.ReadOnly)
+        tear_down = Attribute("tearDown", "Bash script to be executed before
+                releasing the resource", flags = Flags.ReadOnly)
 
         cls._register_attribute(hostname)
         cls._register_attribute(username)
-        cls._register_attribute(password)
+        cls._register_attribute(identity)
+        cls._register_attribute(clean_home)
+        cls._register_attribute(clean_processes)
+        cls._register_attribute(tear_down)
 
     def __init__(self, ec, guid):
         super(LinuxNode, self).__init__(ec, guid)
 
         self._logger = logging.getLogger("neco.linux.Node.%d" % guid)
-        #elf._logger.setLevel(neco.LOGLEVEL)
+
+    def provision(self, filters = None):
+        if not self.api.is_alive():
+            self._state = ResourceState.FAILED
+            self.logger.error("Deploy failed. Unresponsive node")
+            return
+        
+        if self.get("cleanProcesses"):
+            self._clean_processes()
+
+        if self.get("cleanHome"):
+            # self._clean_home() -> this is dangerous
+            pass
 
     def deploy(self):
-        pass
-
-    def discover(self, filters):
-        pass
-
-    def provision(self, filters):
-        pass
-
-    def start(self):
-        pass
-
-    def stop(self):
-        pass
-
-    def deploy(self, group = None):
-        pass
+        self.provision()
+        super(LinuxNode, self).deploy()
 
     def release(self):
-        pass
+        tear_down = self.get("tearDown")
+        if tear_down:
+            self.api.execute(tear_down)
+
+        super(LinuxNode, self).release()
 
     def _validate_connection(self, guid):
         # TODO: Validate!
         return True
 
+    @property
+    def api(self):
+        host = self.get("host")
+        user = self.get("user")
+        identity = self.get("identity")
+        return SSHApiFactory.get_api(host, user, identity)
+
+    def _clean_processes(self):
+        hostname = self.get("hostname")
+        self.logger.info("Cleaning up processes on %s", hostname)
+        
+        cmds = [
+            "sudo -S killall python tcpdump || /bin/true ; "
+            "sudo -S killall python tcpdump || /bin/true ; "
+            "sudo -S kill $(ps -N -T -o pid --no-heading | grep -v $PPID | sort) || /bin/true ",
+            "sudo -S killall -u root || /bin/true ",
+            "sudo -S killall -u root || /bin/true ",
+        ]
+
+        api = self.api
+        for cmd in cmds:
+            out, err = api.execute(cmd)
+            if err:
+                self.logger.error(err)
+            
+    def _clean_home(self):
+        hostname = self.get("hostname")
+        self.logger.info("Cleaning up home on %s", hostname)
+
+         cmds = [
+            "find . -maxdepth 1 ! -name '.bash*' ! -name '.' -execdir rm -rf {} + "
+        ]
+
+        api = self.api
+        for cmd in cmds:
+            out, err = api.execute(cmd)
+            if err:
+                self.logger.error(err)
 
