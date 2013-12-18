9c2d4f6fa604dca0b265cacdf0fdaa8d0ac29442
jtribino <jtribino@localhost>
2013-12-18 17:49:13 +0100

    add bitten into the hook

--- src/nepi/execution/resource.py
+++ src/nepi/execution/resource.py
@@ -980,7 +980,7 @@ class ResourceManager(Logger):
         self.set_state(ResourceState.PROVISIONED, "_provision_time")
 
     def set_state(self, state, state_time_attr):
-        """ Set the state of the RM while saving the time when the state change """
+        """ Set the state of the RM while keeping a trace of the time """
 
         # Ensure that RM state will not change after released
         if self._state == ResourceState.RELEASED:
