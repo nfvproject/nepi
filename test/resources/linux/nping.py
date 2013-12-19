a8f7ebfded78a34c1943d7479e45585bb4bc503a
jtribino <jtribino@localhost>
2013-12-19 14:42:29 +0100

    change the test using slice inria_nepi to the new slice inria_test

--- test/resources/linux/nping.py
+++ test/resources/linux/nping.py
@@ -29,7 +29,7 @@ import unittest
 class LinuxNPingTestCase(unittest.TestCase):
     def setUp(self):
         self.fedora_host = "nepi2.pl.sophia.inria.fr"
-        self.fedora_user = "inria_nepi"
+        self.fedora_user = "inria_test"
 
         self.ubuntu_host = "roseval.pl.sophia.inria.fr"
         self.ubuntu_user = "alina"
