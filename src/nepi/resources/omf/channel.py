b46b7e70cb39a865b5e5e9ed10b0fe70c086cb86
jtribino <julien.tribino@inria.fr>
2014-01-08 16:57:27 +0100

    Change some comments about OMF RM

--- src/nepi/resources/omf/channel.py
+++ src/nepi/resources/omf/channel.py
@@ -38,10 +38,6 @@ class OMFChannel(OMFResource):
         :param creds: Credentials to communicate with the rm (XmppClient for OMF)
         :type creds: dict
 
-    .. note::
-
-       This class is used only by the Experiment Controller through the Resource Factory
-
     """
     _rtype = "OMFChannel"
     _authorized_connections = ["OMFWifiInterface", "OMFNode"]
