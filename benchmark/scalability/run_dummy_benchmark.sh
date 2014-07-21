e341eb331bf4258f5cbd4bf48e754106b9306e85
Alina Quereilhac <alina.quereilhac@inria.fr>
2014-07-21 18:14:52 +0200

    Adding better resource monitoring for the benchmark

--- benchmark/scalability/run_dummy_benchmark.sh
+++ benchmark/scalability/run_dummy_benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## Run instructions:
 ## cd ~/repos/nepi/src/benchmark
-## PYTHONPATH=$PYTHONPATH:~/repos/nepi/src/ bash scalability/run_benchmark.sh
+## PYTHONPATH=$PYTHONPATH:~/repos/nepi/src/ bash scalability/run_dummy_benchmark.sh
 
 
 ## SCALABILTY 
@@ -10,19 +10,21 @@
 run0=1
 runn=10
 nodes=(1 10 100 1000) 
-apps=(1 10 50 100) 
+apps=(1 10 50) 
 threads=(1 10 50 100)  
+delay="0.5"
+opdelay="0"
 
 mkdir -p logs
 
-# Change number of nodes [1, 10, 30, 100, 500] (apps = 1, threads = 20)
+# Change number of nodes, apps, threads
 for n in "${nodes[@]}"; do
     for a in "${apps[@]}"; do
         for t in "${threads[@]}"; do
             for i in $(seq $run0 $runn); do
                 echo "Number of nodes = $n. Number of apps = $a. Number of threads = $t. Run $i."
-                echo "NEPI_LOGLEVEL=debug python scalability/dummy.py -n $n -a $a -t $t -r $i > logs/nodes$n.apps$a.threads$t.runs$i.out 2>&1"
-                NEPI_LOGLEVEL=debug python scalability/dummy.py -n $n -a $a -t $t -r $i > logs/nodes$n.apps$a.threads$t.runs$i.out 2>&1
+                echo "NEPI_LOGLEVEL=debug python scalability/dummy.py -n $n -a $a -t $t -d $delay -o $opdelay -r $i > logs/scheddelay$delay.opdelay$opdelay.nodes$n.apps$a.threads$t.runs$i.out 2>&1"
+                NEPI_LOGLEVEL=debug python scalability/dummy.py -n $n -a $a -t $t -d $delay -o $opdelay -r $i > logs/scheddelay$delay.opdelay$opdelay.nodes$n.apps$a.threads$t.runs$i.out 2>&1
                 if [ $? != 0 ]; then
                     echo "Problem with node $n app $a thread $t execution the $i time"
                 fi
